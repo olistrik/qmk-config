ac37a944cf3a95e08201d9e2643939418b82c38e
Kranex <oliverstrik@gmail.com>
2021-10-18 12:59:43 +0200

    fix planck ez.

--- ez/keymap.c
+++ ez/keymap.c
@@ -126,6 +126,89 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+  rgb_matrix_enable();
+}
+
+#define COLOR_0 {146,244,255}
+#define COLOR_1 {16,255,255}
+
+const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
+    [_QWERTY] = {
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,
+      COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0          ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 },
+
+    [_LOWER] = {
+      COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 },
+
+    [_RAISE] = {
+      COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 },
+
+    [_ADJUST] = {
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
+
+    [_FUNC] = {
+      COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,
+      COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+    HSV hsv = {
+      .h = pgm_read_byte(&ledmap[layer][i][0]),
+      .s = pgm_read_byte(&ledmap[layer][i][1]),
+      .v = pgm_read_byte(&ledmap[layer][i][2]),
+    };
+    if (!hsv.h && !hsv.s && !hsv.v) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        RGB rgb = hsv_to_rgb( hsv );
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+void rgb_matrix_indicators_user(void) {
+  if (keyboard_config.disable_layer_led) { return; }
+  switch (biton32(layer_state)) {
+    case 0:
+      set_layer_color(0);
+      break;
+    case 1:
+      set_layer_color(1);
+      break;
+    case 2:
+      set_layer_color(2);
+      break;
+    case 3:
+      set_layer_color(3);
+      break;
+    case 4:
+      set_layer_color(4);
+      break;
+   default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  }
+}
+
 #ifdef AUDIO_ENABLE
 #endif
 
