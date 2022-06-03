7b66a2cb4b6e135955319086445f545db36a6dd5
Kranex <oliverstrik@gmail.com>
2022-06-03 11:24:55 +0200

    made some changes to the lily keymap

--- lily58l/keymap.c
+++ lily58l/keymap.c
@@ -29,6 +29,10 @@
 #define LOWER MO(_LOWER)
 
 #define FN_TAB LT(_FUNC, KC_TAB)
+#define FN_LOW LT(_LOWER, KC_DOWN)
+#define FN_RAZ LT(_RAISE, KC_UP)
+#define FN_LOW2 LT(_LOWER, KC_LEFT)
+#define FN_RAZ2 LT(_RAISE, KC_RIGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -51,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   FN_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,                    KC_Y   ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_QUOT ,
   KC_LSFT ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                    KC_H   ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_ENT  ,
   KC_LCTRL,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,________,  ________,KC_N   ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
-                            KC_LGUI  ,KC_LALT ,LOWER   ,KC_SPC  ,  KC_SPC  ,RAISE  ,KC_BSPC ,KC_RGUI
+                            KC_LGUI  ,KC_LALT ,FN_LOW  ,KC_SPC  ,  KC_SPC  ,FN_RAZ ,KC_BSPC ,KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -67,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `-----------------------------'         '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,                    KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,
+  ________,________,________,________,________,________,                    ________,________,________,________,________,________,
   KC_TILDE,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,                    KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,________,
-  ________,________,________,________,________,________,                    ________,KC_UNDS ,KC_PLUS ,KC_LCBR ,KC_RCBR ,KC_PIPE ,
-  ________,________,________,________,________,________,________,  ________,________,________,________,________,________,________,
-                             ________,________,________,________,  ________,________,________,________
+  KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,                    ________,KC_UNDS ,KC_PLUS ,KC_LCBR ,KC_RCBR ,KC_PIPE ,
+  KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,________,  ________,________,________,________,________,________,________,
+                             ________,________,________,________,  ________,FN_RAZ2 ,________,________
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -89,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   ________,________,________,________,________,________,                    ________,________,________,________,________,________,
-  ________,________,________,________,________,________,                    ________,________,________,________,________,________,
+  ________,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                    KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,________,
   ________,________,________,________,________,________,                    ________,KC_MINUS,KC_EQUAL,KC_LBRC ,KC_RBRC ,KC_BSLS ,
   ________,________,________,________,________,________,________,  ________,________,________,________,________,________,________,
-                             ________,________,________,________,  ________,________,________,________
+                             ________,________,FN_LOW2 ,________,  ________,________,________,________
 ),
 
 /* FUNC
@@ -247,15 +251,15 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 
 #ifdef OLED_ENABLE
-void oled_task_user(void) {
-
+bool oled_task_user(void) {
   #ifdef TETRIS_ENABLE
   if(tetris_mode()) {
     tetris_tick();
-    return;
+    return false;
   }
   #endif
 
   oled_tick();
+  return false;
 }
 #endif
